971b952f457dac9420ab822f4e2b892b2248be92
Peteris Meiris <petmei01@easv365.dk>
2025-11-24 23:23:44 +0100

    deployment

---
+++ Server/Api/AppOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace Api;
+
+public class AppOptions
+{
+    public string DBConnectionString { get; set; }
+}
--- Server/Api/Program.cs
+++ Server/Api/Program.cs
@@ -1,11 +1,14 @@
+using api;
 using DataAccess;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var appOptions = builder.Services.AddAppOptions(builder.Configuration);
+
 builder.Services.AddDbContext<MyDbContext>(conf =>
 {
-    conf.UseNpgsql();
+    conf.UseNpgsql(appOptions.DBConnectionString);
 });
 
 var app = builder.Build();
