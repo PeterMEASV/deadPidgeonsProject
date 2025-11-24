d35cacea5c880aafa8be962152602dc941f4f856
Peteris Meiris <petmei01@easv365.dk>
2025-11-24 23:00:56 +0100

    updating gitignore

--- Server/Api/Program.cs
+++ Server/Api/Program.cs
@@ -1,4 +1,13 @@
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MyDbContext>(conf =>
+{
+    conf.UseNpgsql();
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
